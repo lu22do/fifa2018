9b1e691bf767e72a8172f8297b79c39db9fc2fc1
Jean-Philippe Cottin <jpcottin@gmail.com>
2018-06-02 13:10:04 -0700

    Change Initial instructions

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -22,15 +22,15 @@ const Welcome = () => (
     <br/>
     <h4>Rules</h4>
     <p>
-    -	Everyone picks any 1 team from each group to form a selection<br/>
+    -	Pick 1 team from each group by clicking on  <Link to="/new-selection">New Selections</Link> <b>". Groups were created following the FIFA ranking before the WC2018<br/>
     -	You can have similar combination of teams in your selection compared to others but you cannot have the exact set if someone else has it already. So, available selections are first-come-first-serve for a selection.<br/>
     -	Each player can make 3 selections such as LudoSet1, LudoSet2, Ludoset3.<br/>
     -	How you get points:<br/>
     &nbsp;&nbsp;o	Your teams will keep gaining points for you as long as they keep playing (not eliminated). Wins, draws and goals get you points.<br/>
     &nbsp;&nbsp;o	Each win gains your selection 3 points, each draw 1 point, each goal scored gains additional Y points (Y = 0.3 during group stage, 0.5 during elimination phase)<br/>
     &nbsp;&nbsp;o	Penalty kicks are not accounted. Only the winner (3 points) and goals during regulation time from each side are counted for points<br/>
-    - After the competition kicks off, it is not possible to add/delete selections.<br/>
-    -	Player with most total points win. In case of a tie, all those folks win.<br/>
+    - After the competition kicks off, it is not possible to create/delete/replace a selection. Your selections can be done until June 13th, just before the opening game of FIFA 2018 <br/>
+    -	The player with most total points win. In case of a tie, all those folks win.<br/>
     </p>
   </div>
 )
